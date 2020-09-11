c95706b150ab6abd39358ddecc86316278f1bc9f
chamzasd <chamzas95@gmail.com>
2020-09-11 12:44:51 -0500

    working Task 1

--- RRT_main.py
+++ RRT_main.py
@@ -9,10 +9,10 @@ print("RRT algorithm!")
 #dimesion grid
 D=[0,100] 
 # iterations
-K = 100
+K = 2000
 #Starting position
 Qstart = [50.0,50.0]
-d = 6
+d = 1
 vertices = []
 edges = []
 
@@ -43,30 +43,35 @@ def getDist(point1,point2):
 def NEAREST_VERTEX(point):
     global vertices
     minDist = 10000.0
-    minId = 0 
+    minId = -1 
     for id_temp in range(len(vertices)):
         dist = getDist(vertices[id_temp], point)
         if(minDist>dist):
             minDist = dist
             minId = id_temp
+    if(minId==-1):
+        print("Error in NEAREST_VERTEX")
     return [minId,minDist]
 
 
 if __name__ == "__main__":
     numpy.random.seed()
     vertices.append(Qstart)
+    # vertices.append([20,50])
     for i in range(K):
         #create random q within D
         Qnew = [numpy.random.rand()*D[1],numpy.random.rand()*D[1]]
-
+        # Qnew = [50,80]
 
         #find nearest neighbor
         data_dist = NEAREST_VERTEX(Qnew)
         Qnear = vertices[data_dist[0]]
 
         #add new vertex
-        x = (Qnear[0] - Qnew[0])/data_dist[1]*d
-        y = (Qnear[1] - Qnew[1])/data_dist[1]*d
+        x2=(Qnear[0] - Qnew[0])/data_dist[1]
+        y2=(Qnear[1] - Qnew[1])/data_dist[1]
+        x = (Qnew[0] - Qnear[0])/data_dist[1]*d
+        y = (Qnew[1] - Qnear[1])/data_dist[1]*d
         if x > d: 
             print ("X Error :" + str(x))
         if y > d: 
